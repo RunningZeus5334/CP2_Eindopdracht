4fe330e57fed0986d989ecd935cbecdc710c3f66
RunningZeus5334 <114076126+RunningZeus5334@users.noreply.github.com>
2023-04-25 15:27:10 +0200

    Litle update
    not fully working yet

--- Code/main.c
+++ Code/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct character {
   uint8_t level;
@@ -25,6 +26,18 @@ void readargv(int argc, char *argv[], struct character *p) {
 
   for (int i = 0; i < argc; i++) {
     printf("%s/n", argv[i]);
+
+    if (strncmp(argv[i], "-s", 2)) {
+      p->spells[0] = atoi(argv[i + 1]);
+      p->spells[1] = atoi(argv[i + 2]);
+      p->spells[2] = atoi(argv[i + 3]);
+      p->spells[3] = atoi(argv[i + 4]);
+      p->spells[4] = atoi(argv[i + 5]);
+      p->spells[5] = atoi(argv[i + 6]);
+      p->spells[6] = atoi(argv[i + 7]);
+      p->spells[7] = atoi(argv[i + 8]);
+      p->spells[8] = atoi(argv[i + 9]);
+      p->spells[9] = atoi(argv[i + 10]);
+    }
   }
-  p->level = 5;
 };
