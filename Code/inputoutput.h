50c7e1a79188c2ed1fa05160b532ba1ebd3f5982
Kobe Dieryck <114076056+TheNeg0t1ator@users.noreply.github.com>
2023-05-30 00:00:58 +0200

    added printhelp function

--- Code/inputoutput.h
+++ Code/inputoutput.h
@@ -7,28 +7,23 @@ printf("=================================================\n");
 
 }
 
+void Printhelp(void)
+
 int StartFunq(int function_call){
 switch (function_call){
     case 505:
-    system("clear");
-    printf("command list:\n");
-    printf("--help\t\thelp menu of command\n\n");
-    printf("spell commands:\n");
-    printf("spell list\t\tlists spells\n");
-    printf("spell next\t\tgoes to the next available spell\n");
-    printf("spell previous\tgoes to the previous available spell\n");
-    printf("spell cast\t\tcasts the currently selected spell, \n\t\t\t\tthis will remove one slot for that spell level\n");
-    printf("\n");
-    printf("for any help regarding a specific function, type --help after it.\n");
+    Printhelp();
     return 0;
     break;    
     case 404:
     system("clear");
     printf("ERROR: 404, cannot find function\n");
     return 1;
+
     break;
     
     default:
+
     break;
   }
   return 1;
@@ -41,13 +36,30 @@ if (strcmp("--help",charbuffer)==0){
 return 505;
 
 
-}else if (strcmp("Spell List",charbuffer)==0){
+}else if (strcmp("spell list",charbuffer)==0){
     system("clear");
     printf("Alright Getting the spell list:");
 return 3;
+}else if(strcmp("spell list",charbuffer)==0){
+
+
 }else{
   return 404;
 }
 
 return 0;
 }
+
+void Printhelp(void){
+    system("clear");
+    printf("command list:\n");
+    printf("--help\t\thelp menu of command\n\n");
+    printf("spell commands:\n");
+    printf("spell list\t\tlists spells\n");
+    printf("spell next\t\tgoes to the next available spell\n");
+    printf("spell previous\tgoes to the previous available spell\n");
+    printf("spell cast\t\tcasts the currently selected spell, \n\t\t\t\tthis will remove one slot for that spell level\n");
+    printf("\n");
+    printf("for any help regarding a specific function, type --help after it.\n");
+}
+
