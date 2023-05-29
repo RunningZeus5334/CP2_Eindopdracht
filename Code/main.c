d6225683bac9ba237d4e8758653106edce9d2f6a
RunningZeus5334 <114076126+RunningZeus5334@users.noreply.github.com>
2023-05-29 23:14:46 +0200

    update ragged array
    linked list failed but json fetch is now compatible with ragged array function

--- Code/main.c
+++ Code/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "json.h"
+#include "inputoutput.h"
+
 
 //ik mag geen c++ dus:
 #define FALSE 0
@@ -18,9 +21,6 @@ string name;
 string url;
 }spellclass;
 
-typedef struct{
-
-}damage;
 
 typedef struct{
 int size;
@@ -28,10 +28,13 @@ effect_area_types type;
 }effectarea;
 
 
+
+
 /*
 the struct for all the spells
 */
 typedef struct{
+string index;
 spellclass spellname;
 string desc;
 string higher_level;
@@ -48,39 +51,32 @@ string attack_type;
 spellclass school;
 spellclass classes;
 spellclass subclasses;
+
 }spell;
 
 
+
 struct character {
-  uint8_t level;
-  uint8_t spells[]; // spell level //amount of spells
-};
+  uint8_t *level;
+  uint8_t *num_spells; // spell level //amount of spells
+  spell spells[];
+}character;
 
-
-struct spellcard {
-  char *spel;
-};
-// structs for the player with pointers
-struct playerstats {
-  int *spellslots[9];
-  int *level;
-  int *spelamount;
-  struct spellcard *amount;
-};
 // make form json file structs
 // read ragged array
-void readragged(int argc, char *argv[], struct playerstats); // read ragged array function
+void readragged(int argc, char *argv[], struct character); // read ragged array function
 void JSON_Fetch(char *spellname /*name of the spell to fetch*/);
 
-
 int main(int argc, char *argv[]) {
-  struct character player;
+  struct character player1;
   
-  struct playerstats player1;
   readragged(argc, &*argv, player1); // reading ragged array
 
-
-  //Read_Input();
+  Greeting(); 
+  int function_called=1;
+  while (function_called){
+  function_called = StartFunq(GetFunq());
+  }
   JSON_Fetch("vicious-mockery");
 
 
@@ -95,7 +91,7 @@ int Read_Input(){
 
 //----------------------------------------
 // read function
-void readragged(int argc, char *argv[], struct playerstats player1) {
+void readragged(int argc, char *argv[], struct character player1) {
 
   for (int i = 0; i < argc; i++) {
 
@@ -106,9 +102,9 @@ void readragged(int argc, char *argv[], struct playerstats player1) {
               (strcmp(argv[j + i], "-h") == 0 || argv[j + i] == NULL)) {
             break;
           }
-          player1.spellslots[j] = calloc(1, sizeof(int));
-          *player1.spellslots[j] = atoi(argv[j + i]);
-          printf("%d\n", *player1.spellslots[j]);
+          player1.num_spells = calloc(1, sizeof(int));
+          player1.num_spells[j] = atoi(argv[j + i]);
+          printf("%d\n", player1.num_spells[j]);
         }
       } else if (strcmp(argv[i], "-l") == 0) {
         int count = 0;
@@ -123,42 +119,22 @@ void readragged(int argc, char *argv[], struct playerstats player1) {
           } else
             count++;
         }
-        player1.spelamount = calloc(1, sizeof(int));
-        *player1.spelamount = count;
-        player1.amount = (struct spellcard *)calloc(*player1.spelamount, sizeof(struct spellcard));
-        for (int j = 2; j <2+ *player1.spelamount; j++) {
-          player1.amount[j].spel = (char *)calloc(strlen(argv[j + i]) + 1, sizeof(char));
-          strcpy(player1.amount[j].spel, argv[j + i]);
-          printf("%s\n", player1.amount[j].spel);
+          //createCircularLinkedList(count);
+          player1.num_spells = calloc(1, sizeof(int));
+          *player1.num_spells = count;
+          
+          for (int j = 2; j <2+ *player1.num_spells; j++) {
+          player1.spells[j].index = (char *)calloc(strlen(argv[j + i]) + 1, sizeof(char));
+          char tmp[50];
+          sscanf(argv[j + i], "%[^.] \0.json", player1.spells[j].index);
+          JSON_Fetch( player1.spells[j].index);
+          printf("%s\n", player1.spells[j].index);
         }
       }
-     else if (strcmp(argv[i], "-h") == 0) {
+      else if (strcmp(argv[i], "-h") == 0) {
       printf("test3\n");
      }
     }
-  }
-}
-
-void JSON_Fetch(char *spellname){
-char CLI_buffer[1000];
-snprintf(CLI_buffer, sizeof(CLI_buffer),"curl https://www.dnd5eapi.co/api/spells/%s", spellname);
-FILE *fp;
-char JSON_FILE[3000];
-fp = popen(CLI_buffer, "r");
-  
-if (fp == NULL) {
-  printf("Error opening CLI_buffer\n");
-  return;
-}else{
-  fgets(JSON_FILE, sizeof(JSON_FILE)-1, fp);
-  //printf(JSON_FILE);
-  system("clear");    
-  FILE *JSON;
-  char filename[30];
-  sprintf(filename, "%s.json", spellname);
-  JSON = fopen("json.json", "w");
-  fprintf(JSON, JSON_FILE);
-  fclose(JSON);
   }
 }
 
